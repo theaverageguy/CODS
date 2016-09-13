5f5781a6ba8841a52ac362c0ebda0cfe406a56b8
Yash <yash14123@iiitd.ac.in>
2016-09-13 12:02:06 +0530

    AddedFiles

---
+++ config.py
@@ -0,0 +1,10 @@
+#--------------------------------------------------------------------------------
+# These tokens are needed for user authentication.
+# Credentials can be generates via Twitter's Application Management:
+#	https://apps.twitter.com/app/new
+#--------------------------------------------------------------------------------
+
+consumer_key = "KsmSmJ3vUffyvkd553X767QTu"
+consumer_secret = "OrdTQNnYtxuPDlt6uYybjjpUbwy4JiwOHDaKkb65mGQRBL1ZbW"
+access_key = "709325032204283904-A3M8Cbv10yn3mcSI9SraEvaMXFTVp3C"
+access_secret = "5oW8soaCwdGCklcC1Xs3tVUs4HfsdKWE5vUtSxRN6L47i"
